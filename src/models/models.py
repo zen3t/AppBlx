9ab679c47f1e23a948908e8329dffb41e6758bdb
zen3t <josealvess6154@gmail.com>
2025-02-26 12:03:37 -0300

    Criando a  base do codigo

---
+++ src/models/models.py
@@ -0,0 +1,24 @@
+from typing import Optional, List
+from pydantic import BaseModel
+
+class Usuario(BaseModel):
+    id:  Optional[str] = None
+    nome: str
+    telefone: str
+    minhas_vendas: List[Pedido]
+    meus_pedidos: List[Pedido]
+class Produto(BaseModel):
+    id:  Optional[str] = None
+    nome: str
+    detalhes: str
+    preco:float
+    disponivel: bool = False
+class Pedido(BaseModel):
+    id: Optional[str] = None
+    usuario: Usuario
+    produto: Produto
+    quantidade: int
+    entrega: bool = True
+    endereco: str
+    observacoes: Optional[str] = 'Sem obeservacoes'
+
