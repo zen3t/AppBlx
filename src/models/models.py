8c492560555ebd996db68a952863e2d584171d7f
zen3t <josealvess6154@gmail.com>
2025-02-26 16:11:36 -0300

    criado as pastas necessaria para o projeto

--- src/models/models.py
+++ src/models/models.py
@@ -5,10 +5,12 @@ class Usuario(BaseModel):
     id:  Optional[str] = None
     nome: str
     telefone: str
+    meus_produtos: List[Produto]
     minhas_vendas: List[Pedido]
     meus_pedidos: List[Pedido]
 class Produto(BaseModel):
     id:  Optional[str] = None
+    usuario: Usuario
     nome: str
     detalhes: str
     preco:float
